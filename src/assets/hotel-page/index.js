4b0acf6652531d02b4c84a86c59db267a21f8867
ZaynAliShan <zainalishann@gmail.com>
2025-02-02 12:11:00 +0500

    Configure Tailwind CSS and HTML template

---
+++ src/assets/hotel-page/index.js
@@ -0,0 +1,24 @@
+import landingBg from "./landing-bg.png"
+import aboutCardImg from "./aboutCardImg.svg"
+import mtn from "./mtn.svg"
+import visa from "./visa.svg"
+import meetingImg from "./meetingImg.png"
+import parkingImg from "./parkingImg.png"
+import swimmingImg from "./swimmingImg.png"
+import wifiImg from "./wifiImg.png"
+import workingImg from "./workingImg.png"
+import yogaImg from "./yogaImg.png"
+
+
+export {
+    landingBg,
+    aboutCardImg,
+    meetingImg,
+    parkingImg,
+    swimmingImg,
+    wifiImg,
+    workingImg,
+    yogaImg,
+    mtn,
+    visa,
+}
