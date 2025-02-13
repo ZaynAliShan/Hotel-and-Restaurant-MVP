cd7be421c59c6253ba607e0696a4e85d19d94191
ZaynAliShan <zainalishann@gmail.com>
2025-02-13 13:17:00 +0500

    Implement gallery and service components

---
+++ src/assets/landing-img/index.js
@@ -0,0 +1,39 @@
+import img1 from './landing-main/img1.png';
+import img2 from './landing-main/img2.png';
+import img3 from './landing-main/img3.png';
+import img4 from './landing-main/img4.png';
+import img5 from './landing-main/img5.png';
+import img6 from './landing-main/img6.png';
+import hotel1 from './hotel-img/hotel-img1.png';
+import hotel2 from './hotel-img/hotel-img2.png';
+import hotel3 from './hotel-img/hotel-img3.png';
+import hotel4 from './hotel-img/hotel-img4.png';
+import hotel5 from './hotel-img/hotel-img5.png';
+import hotel6 from './hotel-img/hotel-img6.png';
+import res1 from './restuarant-img/bwok-restaurants 2.png';
+import res2 from './restuarant-img/heaven-hotel 1.png';
+import res3 from './restuarant-img/inka steak-restaurant 1.png';
+import res4 from './restuarant-img/povoire-noire-restaurant 1.png';
+import res5 from './restuarant-img/soy-asian-restaurant 2.png';
+import res6 from './restuarant-img/sundowner-restaurant 1.png';
+import cofe1 from './cafe-img/cactus-coffee 1.png';
+import cofe2 from './cafe-img/cammellia-coffee 1.png';
+import cofe3 from './cafe-img/indabo-caffee 2.png';
+import cofe4 from './cafe-img/mocha-caffee 1.png';
+import cofe5 from './cafe-img/neo-coffee-shop 1.png';
+import cofe6 from './cafe-img/pishon-caffee 1.png';
+import detailsBg from './details-img/bg-main.jfif';
+import cofeDetails from './details-img/card-cofe.jfif';
+import gallery1 from './details-img/gallery1.jfif';
+import gallery2 from './details-img/gallery2.jfif';
+import gallery3 from './details-img/gallery3.jfif';
+import gallery4 from './details-img/gallery4.jfif';
+import gallery5 from './details-img/gallery5.jfif';
+
+export {
+    img1, img2, img3, img4, img5, img6, // landing main image
+    hotel1, hotel2, hotel3, hotel4, hotel5, hotel6, // hotel page image
+    res1, res2, res3, res4, res5, res6, // restuarant page image
+    cofe1, cofe2, cofe3, cofe4, cofe5, cofe6, // cofe page image
+    detailsBg, cofeDetails, gallery1, gallery2, gallery3, gallery4, gallery5, // details page image
+}
