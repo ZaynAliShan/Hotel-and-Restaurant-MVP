19d3a84e0bd206e5b02f48a12ef98a796de4f17f
ZaynAliShan <zainalishann@gmail.com>
2025-02-15 15:15:00 +0500

    Create payment card components

---
+++ src/assets/loginsignUp/const.js
@@ -0,0 +1,18 @@
+import vector from './Vectors.svg'
+import union from './Union.svg'
+import logo from '../MainAdminDashboard/Component4.svg'
+import menuIcon1 from '../MainAdminDashboard/clarity_list-line.svg'
+import menuIcon2 from '../MainAdminDashboard/clarity_list-line (1).svg'
+import analytic from '../MainAdminDashboard/logos_google-analytics.svg'
+import Vector5 from '../MainAdminDashboard/Vector (5).svg'
+import HomeSVG from '../MainAdminDashboard/Vector (6).svg'
+export{
+    vector,
+    union,
+    logo,
+    menuIcon1,
+    menuIcon2,
+    analytic,
+    Vector5,
+    HomeSVG
+}
